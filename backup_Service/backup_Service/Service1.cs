a2c02bc0d3c014e8d55dd473d757625d172f01f5
Abdul Samad <samadahmed112255@gmail.com>
2018-10-11 23:59:53 +0500

    project upload

---
+++ backup_Service/backup_Service/Service1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace backup_Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public static int time = Int32.Parse(ConfigurationManager.AppSettings["time"]);
+        private Timer timer = null;
+        public Service1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCreateBackup(object sender, ElapsedEventArgs e)
+        {
+            ZipBackup.BackupAndDelete();
+        }
+        
+
+        protected override void OnStart(string[] args)
+        {
+          
+            timer = new Timer();
+            timer.Interval = 1000 * time; // 60 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnCreateBackup);
+            timer.Enabled = true;
+
+        }
+
+        protected override void OnStop()
+        {
+            Console.WriteLine("service has started");
+            timer.Stop();
+            timer = null;
+        }
+    }
+}
---
+++ backup_Service/backup_Service/ZipBackup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Configuration;
+namespace backup_Service
+{
+    class ZipBackup
+    {
+        public static string startndeletePath = ConfigurationManager.AppSettings["from"].ToString();
+        public static string fileName = ConfigurationManager.AppSettings["fileName"].ToString();
+        public static string zipPath = (ConfigurationManager.AppSettings["to"]).ToString();
+
+        public static void BackupAndDelete()
+        {
+            
+          
+            try
+            {
+                CompressFile(startndeletePath, zipPath);
+                DeleteFile(startndeletePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void CompressFile(string pathFrom, string pathTo)
+        {
+            ZipFile.CreateFromDirectory(pathFrom, pathTo + fileName + DateTime.Now.ToString("yyyy-MM-dd h:mm tt"));
+        }
+
+        public static void DeleteFile(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
